4246d3d0fdf87e28c645193d95983829d8c1c4ee
Alek iOS D <alek.juventino@gmail.com>
2020-07-28 01:38:41 +0200

    Initial commit

---
+++ app/src/main/java/com/gsixacademy/android/calculatorgsix/CalculatorActivity.kt
@@ -0,0 +1,88 @@
+package com.android.gsixacademy.calculatorGSIX
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.gsixacademy.android.calculatorgsix.R
+import kotlinx.android.synthetic.main.activity_calculator_layout.*
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator_layout)
+
+
+        initlistener()
+
+    }
+
+    private fun initlistener() {
+
+        button_add.setOnClickListener {
+
+            var firstNumberText = edit_text_number_one.text.toString()
+            var secondNumberText = edit_text_number_two.text.toString()
+
+            if (firstNumberText.isEmpty() || secondNumberText.isEmpty()) {
+                text_view_result_value.text = "0"
+                Toast.makeText(applicationContext, "no value entered", Toast.LENGTH_LONG).show()
+            } else {
+                var result = firstNumberText.toDouble() + Integer.parseInt(secondNumberText)
+                text_view_result_value.text = "$result"
+            }
+
+        }
+
+        button_sub.setOnClickListener {
+            var firstNumberText = edit_text_number_one.text.toString()
+            var secondNumberText = edit_text_number_two.text.toString()
+
+            if (firstNumberText.isEmpty() || secondNumberText.isEmpty()) {
+                text_view_result_value.text = "0"
+                Toast.makeText(applicationContext, "no value entered", Toast.LENGTH_LONG).show()
+            } else {
+                var result = firstNumberText.toDouble() - Integer.parseInt(secondNumberText)
+                text_view_result_value.text = "$result"
+            }
+
+        }
+
+        button_mul.setOnClickListener {
+            var firstNumberText = edit_text_number_one.text.toString()
+            var secondNumberText = edit_text_number_two.text.toString()
+
+            if (firstNumberText.isEmpty() || secondNumberText.isEmpty()) {
+                text_view_result_value.text = "0"
+                Toast.makeText(applicationContext, "no value entered", Toast.LENGTH_LONG).show()
+            } else {
+                var result = firstNumberText.toDouble() * Integer.parseInt(secondNumberText)
+                text_view_result_value.text = "$result"
+            }
+
+        }
+
+        button_div.setOnClickListener {
+            var firstNumberText = edit_text_number_one.text.toString()
+            var secondNumbertext = edit_text_number_two.text.toString()
+
+            if (firstNumberText.isEmpty() || secondNumbertext.isEmpty()) {
+                text_view_result_value.text = "/"
+                Toast.makeText(applicationContext, "no value entered", Toast.LENGTH_LONG).show()
+            } else if (secondNumbertext.toInt() == 0) {
+                text_view_result_value.text = "/"
+                Toast.makeText(
+                    applicationContext,
+                    "second number must not be '0' - Error",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                var result = firstNumberText.toDouble() / secondNumbertext.toInt()
+                text_view_result_value.text = "$result"
+            }
+        }
+
+    }
+
+}
+
